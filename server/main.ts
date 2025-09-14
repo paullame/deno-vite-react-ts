5e77420a0e3fa525c676cc98c2fd27c2c64ac65f
Paul Lame <p.lame@icloud.com>
2025-09-14 14:50:40 +0200

    lint: user deno import map instead of jsr: specifier directly

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,5 @@
-import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
+import { Application } from "@oak/oak/application";
+import { Router } from "@oak/oak/router";
 import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
 
 export const app = new Application();
